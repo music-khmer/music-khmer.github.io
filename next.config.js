5187e78e4c5de989f2a852cf9d61a43a2e32b44e
ravuthz <ravuthz@gmail.com>
2020-10-26 22:44:07 +0700

    Deploy Next App to GitHub Pages - /docs

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    basePath: '',
+    assetPrefix: ''
+    // exportPathMap: async function (
+    //   defaultPathMap,
+    //   { dev, dir, outDir, distDir, buildId }
+    // ) {
+    //   return {
+    //     '/': { page: '/' },
+    //     '/api/hello': { page: '/pages/api/hello' },
+    //   }
+    // },
+  }
